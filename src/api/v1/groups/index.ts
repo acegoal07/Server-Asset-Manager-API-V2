0602e004b8faba36f4af6ec97c2b721fa38d4961
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2026-08-13 15:20:52 +0100

    Update index.ts

--- src/api/v1/groups/index.ts
+++ src/api/v1/groups/index.ts
@@ -5,9 +5,10 @@ import createGroup from './controllers/createGroup';
 import getGroupById from './controllers/getGroupById';
 import updateGroup from './controllers/updateGroup';
 import extendGroup from './controllers/extendGroup';
+import initialiseGroupNodes from './controllers/initialiseGroupNodes';
 
 export default new Hono()
-   .route('/initialise')
+   .route('/initialise', initialiseGroupNodes)
    .route('/:id/extend', extendGroup)
    .route('/:id', updateGroup)
    .route('/:id', getGroupById)
