ed9fe833e780e5343eeb5773f70aafd60eafd5fa
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2026-08-13 09:54:30 +0100

    More

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -3,4 +3,5 @@ import { Hono } from 'hono';
 export default new Hono()
    .route('/assets', (await import('./assets')).default)
    .route('/groups', (await import('./groups')).default)
-   .route('/storages', (await import('./storages')).default);
+   .route('/storages', (await import('./storages')).default)
+   .route('/storages', (await import('./types')).default);
