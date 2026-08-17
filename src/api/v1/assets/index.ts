253bdfef53f8b6c0cb0e07950a068882a4b61e9a
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2026-08-17 09:46:28 +0100

    Update index.ts

--- src/api/v1/assets/index.ts
+++ src/api/v1/assets/index.ts
@@ -4,9 +4,11 @@ import getAssetById from './controllers/getAssetById';
 import getAssets from './controllers/getAssets';
 import createAsset from './controllers/createAsset';
 import deleteAsset from './controllers/deleteAsset';
+import updateAsset from './controllers/updateAsset';
 
 export default new Hono()
    .route('/:id', getAssetById)
    .route('/:id', deleteAsset)
+   .route('/:id', updateAsset)
    .route('/', getAssets)
    .route('/', createAsset);
